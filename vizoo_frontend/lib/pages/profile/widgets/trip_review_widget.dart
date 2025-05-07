6fd6e065f19185e12acfbe9fd8b6db98e544a380
VanHuy <2251120293@ut.edu.vn>
2025-05-07 10:16:47 +0700

    fix profile v2

--- vizoo_frontend/lib/pages/profile/widgets/trip_review_widget.dart
+++ vizoo_frontend/lib/pages/profile/widgets/trip_review_widget.dart
@@ -1,4 +1,3 @@
-// lib/pages/profile/widgets/trip_review_widget.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
